ce857d25d829c3e17229e785b106761d06e7c823
John Matula <john@johnmatu.la>
2018-10-22 23:29:48 -0400

    Add OpenWeather API

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,7 @@
 var requests = false;
 var timeout;
+var weatherapikey = false;
 var controlOutlets = function(btn, off) {
-
 	$.ajaxQueue({
 		'url': 'php/status.php',
 		'data': {
@@ -14,6 +14,19 @@ var controlOutlets = function(btn, off) {
 		queryOutletStatus();
 	});
 };
+var updateWeatherFirstTime = function() {
+	$.ajax({
+		url: 'php/apikey.php',
+		success: function(data) {
+			weatherapikey = data;
+			updateWeather();
+		},
+		timeout: 1000,
+		error: function(jqXHR, textStatus, errorThrown) {
+			console.warn("Status JSON returned error: " + errorThrown);
+		}
+	});
+}
 
 // Don't get the PHP engine involved for querying the outlet status. This will
 // lessen the load on the Raspberry Pi.
@@ -38,18 +51,85 @@ var updateUI = function(data) {
 	Object.keys(data.rooms).forEach(function (outlet) {
 		$(".room[data-outlets=" + outlet + "]").attr("data-status", (data.rooms[outlet] ? "on" : "off"));
 	});
-
-	if(data.behavior == "additive") {
-		$("button[data-actiontype=behavior]").html('<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" 	 width="20px" height="20px" viewBox="0 0 20 20" enable-background="new 0 0 20 20" xml:space="preserve"> <line fill="none" stroke="#414042" stroke-width="2" stroke-miterlimit="10" x1="1" y1="12" x2="11" y2="2"/> <polyline fill="none" stroke="#414042" stroke-width="2" stroke-miterlimit="10" points="11,8 11,2 5,2 "/> <line fill="none" stroke="#414042" stroke-width="2" stroke-miterlimit="10" x1="8" y1="19" x2="18" y2="9"/> <polyline fill="none" stroke="#414042" stroke-width="2" stroke-miterlimit="10" points="18,15 18,9 12,9 "/> </svg> ');
-	} else {
-		$("button[data-actiontype=behavior]").html('<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" 	 width="20px" height="20px" viewBox="0 0 20 20" enable-background="new 0 0 20 20" xml:space="preserve"> <g> 	<line fill="none" stroke="#414042" stroke-width="2" stroke-miterlimit="10" x1="2" y1="1.668" x2="8" y2="7.668"/> 	<polyline fill="none" stroke="#414042" stroke-width="2" stroke-miterlimit="10" points="12,18 18,18 18,12 	"/> 	<line fill="none" stroke="#414042" stroke-width="2" stroke-miterlimit="10" x1="2" y1="17.668" x2="18" y2="1.668"/> 	<polyline fill="none" stroke="#414042" stroke-width="2" stroke-miterlimit="10" points="18,8 18,2 12,2 	"/> 	<line fill="none" stroke="#414042" stroke-width="2" stroke-miterlimit="10" x1="12" y1="11.668" x2="18" y2="17.668"/> </g> </svg> ');
-	}
 };
 
 var refreshClock = function() {
 	$('.clock__time').html(moment().format('h:mm'));
 	$('.clock__weekday').html(moment().format('dddd'));
 	$('.clock__date').html(moment().format('D MMMM'));
+}
+
+var updateWeather = function() {
+	var lat = 40.780366;
+	var lon = -73.948188;
+
+	var weatherRequest = $.ajax({
+		url: 'https://api.openweathermap.org/data/2.5/weather?lat=40.780366&lon=-73.948188&appid=' + weatherapikey + '&units=imperial',
+		success: function(data) {
+			$(".weather__temp").text(Math.round(data.main.temp) + "°");
+			$(".weather__humidity").text("Humidity " + Math.round(data.main.humidity) + "%");
+
+			var feels = calculateFeels(data.main.temp, data.main.humidity, data.wind.speed);
+			if(feels) {
+				$(".weather__feels").text("Feels like " + Math.round(feels) + "°")
+			} else {
+				$(".weather__feels").text();
+			}
+
+			if(data.wind.speed >= 0.5) {
+				$(".weather__wind").text("Wind " + Math.round(data.wind.speed)).append(" <span class='weather__windunit'>MPH</span>");
+			} else {
+				$(".weather__wind").text("Wind is calm");
+			}
+		},
+		timeout: 3000,
+		error: function(jqXHR, textStatus, errorThrown) {
+			console.warn("Weather returned error: " + errorThrown);
+		}
+	});
+}
+
+function calculateFeels(temperature, humidity, wind) {
+	var feel = false;
+
+	if(temperature >= 75) {
+		feel = calculateHeatIndex(temperature, humidity);
+
+		if(feel <= temperature) {
+			feel = false;
+		}
+	} else if(temperature <= 55 && wind >= 3){
+		feel = calculateWindChill(temperature, wind);
+
+		if(feel >= temperature) {
+			feel = false;
+		}
+	}
+
+	return feel;
+}
+
+function calculateHeatIndex(T, RH){
+	// From http://www.hpc.ncep.noaa.gov/html/heatindex_equation.shtml
+	var estimation = HI = 0.5 * (T + 61.0 + ((T-68.0)*1.2) + (RH*0.094));
+
+	if(((estimation + T) / 2) < 80){
+		return T;
+	}
+
+	var HI = -42.379 + (2.04901523*T) + (10.14333127*RH) - (.22475541*T*RH) - (.00683783*T*T) - (.05481717*RH*RH) + (.00122874*T*T*RH) + (.00085282*T*RH*RH) - (.00000199*T*T*RH*RH);
+
+	if(T >= 80 && T <= 112 && RH < 13) {
+		HI -= ((13-RH)/4)*Math.sqrt((17-Math.abs(T-95.))/17);
+	} else if(T >= 80 && T <= 87 && RH > 85) {
+		HI += ((RH-85)/10) * ((87-T)/5)
+	}
+
+	return Math.round(HI);
+}
+
+function calculateWindChill(T, V){
+	return Math.round(35.74 + (0.6215*T) - (35.75*Math.pow(V,0.16)) + (0.4275*T*Math.pow(V,0.16)));
 }
 
 // Document is ready, do this stuff.
@@ -83,6 +163,9 @@ $(function() {
 
 	refreshClock();
 	var intervalClock = setInterval(refreshClock, 2500);
+
+	updateWeatherFirstTime();
+	var intervalWeather = setInterval(updateWeather, (5 * 60 * 1000));
 });
 
 // Handles quick button taps by queueing them.
